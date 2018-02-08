62a30ad00706f83e87aca627a327a69478c0d41c
Tanawat Polsuwan <tanawat.polsuwan@gmail.com>
2018-02-08 17:09:06 +0700

    Initial Commit

---
+++ TouchIDExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchIDExample
+//
+//  Created by macbook on 2/8/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchIDExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchIDExample
+//
+//  Created by macbook on 2/8/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
